b789af35967e6e67bb50caa76a802b1add4a317f
BackRankCheckMate <143332200+BackRankCheckMate@users.noreply.github.com>
2023-12-25 20:33:21 +0545

    Adding Doubly LinkedList

--- Algorithms/Searching/BinarySearchDemo.java
+++ Algorithms/Searching/BinarySearchDemo.java
@@ -1,15 +1,17 @@
 package Searching;
 
+// This search method only works in an already sorted array
+
 class SearchBinary {
     public static boolean search( int arr[], int key ) {
-        int low = 0, high = arr.length - 1, mid;
-        boolean found = false;
+        int low = 0, high = arr.length - 1, mid;    //  Initializing required variables
+        boolean found = false;  //  Indicates when the key is found, used to end the process
         while ( low <= high ) {
             mid = ( low + high ) / 2;
             if ( key < arr[mid] ) 
-                high = mid - 1;
+                high = mid - 1;     // further dividing the search space
             else if ( arr[mid] < key ) 
-                low = mid + 1;
+                low = mid + 1;      // further dividing the search space
             else {
                 found = true;
                 return found;    
